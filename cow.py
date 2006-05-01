6cde2fd70cc6e770411a372d69fdcede3792c705
b4d <b4d@sablun.org>
2006-05-01 22:13:13 +0200

    Cow.

---
+++ cow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+author = 'b4d, www.baccic.be'
+version = '0.1a, May 1, 2006'
+#
+#TODO:
+#       -maybe input parameters, and different cows :)
+
+say = "*MOOOOOoooooOO*"
+cow = ("         (__)  O",
+       "         (xx) o ",
+       "   /------\/",
+       "  /|     ||",
+       " * ||----||",
+       "   ~~    ~~")
+print "              ( ",say," )"
+for i in cow:
+    print i
