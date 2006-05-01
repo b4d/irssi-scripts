af27bd3b8739e528e74b42efa1213e9d4121d947
b4d <b4d@sablun.org>
2006-05-01 22:13:13 +0200

    Fish

---
+++ fish.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+author = 'b4d, www.baccic.be'
+version = '0.1a, May 1, 2006'
+
+#
+#TODO:
+#       -don't know :D
+
+say = " *WTF WTF*"
+fish = "     ><> o O "
+print fish, say
+
